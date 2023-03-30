dbb63f4173892a2c2b4c8677383a767e914b2b80
Leandro Silva Araujo <leandro.silva@acocearense.com.br>
2023-03-30 18:56:32 -0300

    Remoção de informações

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "leandro2293@gmail.com",
-    pass: "xoqjlnwlunqaalfv"
+    user: "",
+    pass: ""
   },
 });
 
